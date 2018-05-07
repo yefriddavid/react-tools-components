90fb01ee811cb87e1e6f5d7c32d6d28cf31ac5a3
Jeffrey Rios <yefriddavid@gmail.com>
2018-05-07 17:45:50 -0300

    mejoras

--- src/index.js
+++ src/index.js
@@ -2,31 +2,12 @@ import React from 'react';
 
 const FriendlyRoutes = ({ component: Component, ...rest }) => {
   let element
-
-  if(fakeAuth.isAuthenticated(rest)){
-    if(rest.accessState === true)
-      element = (<Component {...rest} />)
-    else
-      element = (<Redirect to={{pathname: `/guest/accessing`, state: { from: rest.location }}} />)
-  }else{
-    if(rest.location.search.indexOf("p=") > 0){
-      element = (<Redirect to={{ pathname: '/guest/nut', state: { from: rest.location }}} />)
-    else{
-      element = (<Redirect to={{ pathname: '/auth/login'}} />)
-    }
-  }
-
+  element = (<Component {...rest} />)
   return (
     <Route {...rest} render={ (props) => (element)} />
   )
 }
 
-FriendlyRoutes.propTypes = {
-  accessState: PropTypes.bool.isRequired
-}
 
-const mapStateToProps = state => ({
-  accessState: state.auth.access.success
-})
-
-export default connect(mapStateToProps)(FriendlyRoutes)
+export default FriendlyRoutes
+//export default connect(mapStateToProps)(FriendlyRoutes)
